807a3abaa0cf460958baccd2d08b7201dd8377a1
Zoolian <m@zooil.com>
2020-01-24 17:10:52 +0900

    update

--- plugins/environment_variables.rb
+++ plugins/environment_variables.rb
@@ -2,7 +2,7 @@ module Jekyll
   class EnvironmentVariablesGenerator < Generator
     def generate(site)
       # https://www.netlify.com/docs/continuous-deployment/#build-environment-variables
-      repo_url = ENV['REPOSITORY_URL'] || 'https://github.com/hakorea/hakorea.github.io'
+      repo_url = 'https://github.com/hakorea/gitpages_source'
 
       # Rewrite urls if repo url is the ssh format.
       if repo_url.start_with? 'git@github.com:'
